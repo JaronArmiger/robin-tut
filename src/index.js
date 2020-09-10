368f601211ae998e5061c52deb224ba20777b5b3
JaronArmiger <jaron.armiger@gmail.com>
2020-09-10 17:49:16 -0500

    add root route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 
-csont app = express();
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('sup MFS');
+});
 
 app.listen(3000, () => {
   console.log('fuck outta here 3000');
