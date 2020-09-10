fc4ca22ef5ab1ea5bcbd7df657662715145eff82
JaronArmiger <jaron.armiger@gmail.com>
2020-09-10 18:05:11 -0500

    add post, put, delete methods

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('sup MFS');
+  res.send('received a GET request');
 });
 
-app.listen(3000, () => {
+app.post('/', (req, res) => {
+ res.send('received a POST request');
+});
+
+app.put('/', (req, res) => {
+ res.send('received a PUT request');
+});
+
+app.delete('/', (req, res) => {
+ res.send('received a DELETE request');
+});
+
+app.listen(port, () => {
   console.log('fuck outta here 3000');
 });
