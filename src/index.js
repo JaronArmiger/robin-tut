ab41a2155e33dffce5fe5ec3dac4d0f7a308e0b4
JaronArmiger <jaron.armiger@gmail.com>
2020-09-11 15:37:06 -0500

    move data to separate module and adjust route logic accordingly

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import models from './models';
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -11,53 +12,32 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-let users = {
-  1: {
-  	id: '1',
-  	username: 'Khomeini',
-  },
-  2: {
-  	id: '2',
-  	username: 'Khamenei',
-  },
-};
-
-let messages = {
-  1: {
-  	id: '1',
-  	text: 'bonyads',
-  	userId: '1'
-  },
-  2: {
-  	id: '2',
-  	text: 'zurkhana',
-  	userId: '2'
-  },
-};
-
 app.use((req, res, next) => {
-  req.me = users['1'];
+  req.context = {
+  	models,
+  	me: models.users['1'],
+  }
   next();
 });
 
 app.get('/session', (req, res) => {
-  return res.send(users[req.me.id]);
+  return res.send(req.context.models.users[req.context.me.id]);
 });
 
 app.get('/users', (req, res) => {
-  res.send(Object.values(users));
+  return res.send(Object.values(req.context.models.users));
 });
 
 app.get('/users/:userId', (req, res) => {
-  res.send(users[req.params.userId]);
+  return res.send(req.context.models.users[req.params.userId]);
 });
 
 app.get('/messages', (req, res) => {
-  res.send(Object.values(messages));
+  return res.send(Object.values(req.context.models.messages));
 });
 
 app.get('/messages/:messageId', (req, res) => {
-  res.send(messages[req.params.messageId]);
+  return res.send(req.context.models.messages[req.params.messageId]);
 });
 
 app.post('/messages', ( req, res) => {
@@ -65,16 +45,16 @@ app.post('/messages', ( req, res) => {
   const message = {
   	id,
   	text: req.body.text,
-  	userId: req.me.id,
+  	userId: req.context.me.id,
   };
 
-  messages[id] = message;
+  req.context.models.messages[id] = message;
   return res.send(message);
 });
 
 app.put('/messages/:messageId', (req, res) => {
-  const message = messages[req.params.messageId];
-  if (message.userId === req.me.id) {
+  const message = req.context.models.messages[req.params.messageId];
+  if (message.userId === req.context.me.id) {
   	message.text = req.body.text;
   	res.send(message);
   } else {
@@ -86,9 +66,9 @@ app.delete('/messages/:messageId', (req, res) => {
   const {
   	[req.params.messageId]: message,
   	...otherMessages
-  } = messages;
+  } = req.context.models.messages;
 
-  messages = otherMessages;
+  req.context.models.messages = otherMessages;
   return res.send(message);
 });
 
